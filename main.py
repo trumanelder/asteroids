23b2bb38306f75e64c6ad7d26ad5ef8e5fc2ff8f
Truman <truman.t.elder@gmail.com>
2026-08-13 11:29:45 -0400

    add pygame clock for delta time tracking

--- main.py
+++ main.py
@@ -4,18 +4,28 @@ from constants import SCREEN_WIDTH
 from logger import log_state
 
 def main():
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
+
         log_state()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill("black")
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
